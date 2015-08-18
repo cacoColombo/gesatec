3b117d5dec01f220193f0febfa4fca75dc9b1efc
gutorockstar <augustoalvess@hotmail.com>
2015-08-18 00:36:37 -0300

    Adicionado

---
+++ src/modulo/cadastro/dao/CertificacaoDAO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modulo.cadastro.dao;
+
+import modulo.sistema.dao.DAO;
+
+/**
+ *
+ * @author augusto
+ */
+public class CertificacaoDAO extends DAO
+{
+}
