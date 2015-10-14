16aaa84461f542acd7a834ac77f719676d21156c
gutorockstar <augustoalvess@hotmail.com>
2015-10-14 19:23:57 -0300

    Ajustado

--- src/META-INF/gesatec.sql
+++ src/META-INF/gesatec.sql
@@ -1,5 +1,11 @@
 BEGIN;
 
+--Usuário admin padrão
+INSERT INTO useraccount (login, password, name, active) VALUES ('admin', MD5('admin'), 'Augusto Alves da Silva', true);
+INSERT INTO pessoa (id, nome, telefonecelular, usuario_id, tipo, sexo) VALUES (1, 'Augusto Alves da Silva', '(51) 9241-3296', 1, 'cliente', 'M');
+INSERT INTO cliente (id) VALUES (1);
+ALTER SEQUENCE pessoa_id_seq RESTART WITH 2;
+
 INSERT INTO diadasemana (id, nome, abreviatura) VALUES (1, 'Domingo', 'Dom'), (2, 'Segunda-feira', 'Seg'), (3, 'Terça-feira', 'Ter'), (4, 'Quarta-feira', 'Qua'), (5, 'Quinta-feira', 'Qui'), (6, 'Sexta-feira', 'Sex'), (7, 'Sábado', 'Sáb');
 INSERT INTO tipoderepeticao (id, nome) VALUES (1, 'Diária'), (2, 'Semanal'), (3, 'Mensal'), (4, 'Anual');
 INSERT INTO statusagenda (id, nome) VALUES (1, 'AGENDADO'), (2, 'CONFIRMADO'), (3, 'EM ANDAMENTO'), (4, 'FINALIZADO');
